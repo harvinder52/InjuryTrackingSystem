9bd7f4f11253b5b59d689f1dc85e876d9e041946
Harv <85876365+harvinder52@users.noreply.github.com>
2023-10-21 12:19:31 +0530

    clickable human body

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 
+import HumanBodyMap from "./components/HumanBodyMap";
+import ThreeDModelViewer from "./components/3dhumanbody";
+
 export default function Index() {
   const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+        <HumanBodyMap />
+      </div>
+    );
+  }
 
   return <a href="/api/auth/login">Login</a>;
 }
